f3814799562eccbdac1a22a69153d9b443f60341
marcus-nguyen <imkay2712@gmail.com>
2025-05-26 22:22:14 +0700

    finish intro

--- page-templates/home-page.php
+++ page-templates/home-page.php
@@ -11,6 +11,7 @@ get_template_part('sections/common/slider-top');
 <div class="home-cover-1">
     <?php
     get_template_part('sections/home/service');
+    get_template_part('sections/home/intro');
     ?>
 </div>
 <?php get_footer(); ?>
